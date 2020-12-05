a40f865aa9175dd1873704fe30c86aab9d509bd9
Lucas Kempe <lucas.kempe@icloud.com>
2020-12-05 14:27:01 -0300

    New index.js file update...

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 function Greeting() {
-    return <h4>This is a component</h4>;
+    return (
+        <div>
+            <h3>hello world</h3>
+            <ul>
+                <li>
+                    <a href = '#' >Hey there</a>
+                </li>
+            </ul>
+        </div>
+    );
 }
+
+// const Greeting = () => {
+//     return React.createElement(
+//         'div',
+//         {},
+//         React.createElement('h1', {}, 'hello world')
+//     );
+// }
 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
