3d643f6a96324d4bda75f9c17d6f70635eff6881
Lucas Kempe <lucas.kempe@icloud.com>
2020-12-11 18:58:54 -0300

    Now the registration form has a submit btn and some syle...

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -5,17 +5,18 @@ class SignUp extends Component {
     render() {
         return (
             <div className="SignUp">
-                <h1>Registrate</h1>
+                <h1>Register</h1>
                 <form className="Form">
-                    <label for="username">Nombre de usuario: </label>
+                    <label for="username">Username: </label>
                     <input type="text" id="name" name="name" required></input>
 
                     <label for="email">Email: </label>
                     <input type="text" id="email" name="password" required></input>
 
-                    <label for="password">Contraseña: </label>
+                    <label for="password">Password: </label>
                     <input type="password" id="password" name="password" required></input>
-                </form>
+                    <button type = "submit">Register now</button>
+                </form> 
             </div>
         );
     }
